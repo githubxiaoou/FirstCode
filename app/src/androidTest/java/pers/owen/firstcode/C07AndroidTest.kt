c003eb167349aa8b20820d45ca6d582094be18ff
swo <sunwenou@qq.com>
2021-04-13 08:03:59 +0800

    第七章完

--- app/src/androidTest/java/pers/owen/firstcode/C07AndroidTest.kt
+++ app/src/androidTest/java/pers/owen/firstcode/C07AndroidTest.kt
@@ -81,43 +81,5 @@ class C07AndroidTest : ExampleInstrumentedTest() {
 //        db.insert("Book", null, values)
     }
 
-//    fun cvOf(vararg pairs: Pair<String, Any?>): ContentValues {
-//        val cv = ContentValues()
-//        for (pair in pairs) {
-//            val key = pair.first
-//            val value = pair.second
-//            when (value) {
-//                is Int -> cv.put(key, value)
-//                is Long -> cv.put(key, value)
-//                is Short -> cv.put(key, value)
-//                is Float -> cv.put(key, value)
-//                is Double -> cv.put(key, value)
-//                is Boolean -> cv.put(key, value)
-//                is String -> cv.put(key, value)
-//                is Byte -> cv.put(key, value)
-//                is ByteArray -> cv.put(key, value)
-//                null -> cv.putNull(key)
-//            }
-//        }
-//        return cv
-//    }
 
-    fun cvOf(vararg pairs: Pair<String, Any?>) = ContentValues().apply {
-        for (pair in pairs) {
-            val key = pair.first
-            val value = pair.second
-                when (value) {
-                is Int -> put(key, value)
-                is Long -> put(key, value)
-                is Short -> put(key, value)
-                is Float -> put(key, value)
-                is Double -> put(key, value)
-                is Boolean -> put(key, value)
-                is String -> put(key, value)
-                is Byte -> put(key, value)
-                is ByteArray -> put(key, value)
-                null -> putNull(key)
-            }
-        }
-    }
 }
